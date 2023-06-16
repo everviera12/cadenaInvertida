9b3a7879e7196d1d9f49264be98cc9d923e54ba4
EverViera12 <evertigre10@gmail.com>
2023-06-15 21:31:02 -0600

    Agregar archivos de proyecto.

---
+++ cadenaInvertida/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cadenaInvertida
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un texto:");
+            string texto = Console.ReadLine();
+
+            string textoInvertido = InvertirCadena(texto);
+
+            Console.WriteLine("Texto invertido: " + textoInvertido);
+            Console.ReadLine();
+        }
+
+          static string InvertirCadena(string texto)
+        {
+            char[] caracteres = texto.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
